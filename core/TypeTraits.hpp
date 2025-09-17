2a4023481f322072f6f17c7946e205377e13b52a
Kiju <dev@kiju.page>
2025-09-17 09:04:52 +0200

    Sprinkle in some template magic

---
+++ core/TypeTraits.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <type_traits>
+
+namespace core_utils {
+
+template <class T, class = void>
+struct is_noarg_callable_t : public std::false_type {};
+
+template <class T>
+struct is_noarg_callable_t<T, decltype(std::declval<T &&>()())>
+    : public std::true_type {};
+
+template <class T>
+struct returns_void_t
+    : public std::is_same<void, decltype(std::declval<T &&>()())> {};
+
+template <class A, class B, class... Rest>
+struct and_t : public and_t<A, and_t<B, Rest...>> {};
+
+template <class A, class B>
+struct and_t<A, B> : public std::conditional<A::value, B, A>::type {};
+
+template <class T>
+struct is_proper_scope_guard_callback_t
+    : public and_t<is_noarg_callable_t<T>, returns_void_t<T>,
+                   std::is_nothrow_destructible<T>> {};
+} // namespace core_utils
