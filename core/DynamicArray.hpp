a17c2bbd9f931a232c75260a36fa68b4064b1c75
Kiju <dev@kiju.page>
2025-09-19 13:19:41 +0200

    Implement access methods

--- core/DynamicArray.hpp
+++ core/DynamicArray.hpp
@@ -2,11 +2,13 @@
 // Licensed under the MIT License.
 // See LICENSE.md file in the project root for full license information.
 
+#include "TypeTraits.hpp"
 #include <concepts>
 #include <cstddef>
 #include <initializer_list>
 #include <limits>
 #include <memory>
+#include <stdexcept>
 
 namespace core {
 template <class T, class Allocator = std::allocator<T>>
@@ -113,21 +115,31 @@ public:
     // Info
     // =========================================================================
 public:
-    bool is_empty() const noexcept { return size() == 0; }
+    [[nodiscard]] bool is_empty() const noexcept { return size() == 0; }
 
-    bool is_full() const noexcept { return size() = capacity(); }
+    [[nodiscard]] bool is_full() const noexcept { return size() = capacity(); }
 
-    size_type capacity() const noexcept {
+    [[nodiscard]] size_type capacity() const noexcept {
         return std::distance(m_first, m_capacity);
     }
 
-    constexpr size_type max_capacity() const noexcept {
-        return std::numeric_limits<size_type>::max();
+    [[nodiscard]] constexpr size_type max_capacity() const noexcept {
+        return max_capacity();
     }
 
-    size_type size() const noexcept { return std::distance(m_first, m_last); }
+    [[nodiscard]] size_type size() const noexcept {
+        return std::distance(m_first, m_last);
+    }
 
-    Allocator allocator() const noexcept {
+    [[nodiscard]] constexpr size_type max_size() const noexcept {
+        if constexpr (core_type::HasMaxSize<Allocator>) {
+            return AT::max_size();
+        } else {
+            return std::numeric_limits<size_type>::max();
+        }
+    }
+
+    [[nodiscard]] Allocator allocator() const noexcept {
         return std::min(
             AT::max_size(m_allocator), std::numeric_limits<size_type>::max());
     }
@@ -136,16 +148,74 @@ public:
     // Access
     // =========================================================================
 
+    reference operator[](size_type i) {
+        return const_cast<reference>(
+            static_cast<const DynamicArray&>(*this).impl_bound_checked_at(i));
+    }
+    const_reference operator[](size_type i) const {
+        return impl_bound_checked_at(i);
+    }
+
+    reference at(size_type i) {
+        return const_cast<reference>(
+            static_cast<const DynamicArray&>(*this).impl_bound_checked_at(i));
+    }
+    const_reference at(size_type i) const { return impl_bound_checked_at(i); }
+
+    reference       unsafe_at(size_type i) noexcept { return m_first[i]; }
+    const_reference unsafe_at(size_type i) const noexcept { return m_first[i]; }
+
+    reference front() {
+        return const_cast<reference>(
+            static_cast<const DynamicArray&>(*this).impl_bound_checked_front());
+    }
+    const_reference front() const { return impl_bound_checked_front(); }
+
+    reference       unsafe_front() noexcept { return *m_first; }
+    const_reference unsafe_front() const noexcept { return *m_first; }
+
+    reference back() {
+        return const_cast<reference>(
+            static_cast<const DynamicArray&>(*this).impl_bound_checked_back());
+    }
+    const_reference back() const { return impl_bound_checked_back(); }
+
+    reference       unsafe_back() noexcept { return m_last[-1]; }
+    const_reference unsafe_back() const noexcept { return m_last[-1]; }
+
+    pointer       raw_data() noexcept { return m_first; }
+    const_pointer raw_data() const noexcept { return m_first; }
+
     // =========================================================================
     // Modifiers
     // =========================================================================
 
     // =========================================================================
-    // Memory management
+    // Impl
     // =========================================================================
+private:
+    inline const_reference impl_bound_checked_at(size_type i) const {
+        if (i >= size()) [[unlikely]] {
+            throw std::out_of_range("DynamicArray => Index out of range.");
+        }
 
-    // =========================================================================
-    // Utils
-    // =========================================================================
+        return m_first[i];
+    }
+
+    inline const_reference impl_bound_checked_front() const {
+        if (is_empty()) [[unlikely]] {
+            throw std::out_of_range("DynamicArray => Array is empty.");
+        }
+
+        return *m_first;
+    }
+
+    inline const_reference impl_bound_checked_back() const {
+        if (is_empty()) [[unlikely]] {
+            throw std::out_of_range("DynamicArray => Array is empty.");
+        }
+
+        return m_last[-1];
+    }
 };
 }
