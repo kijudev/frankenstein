9d84c9030345ba233455a7700b1472af8ecf975f
Kiju <dev@kiju.page>
2025-09-24 17:02:03 +0200

    Add a test case for core::DynamicArray

---
+++ tests/DynamicArray.cpp
@@ -0,0 +1,16 @@
+#include <cstddef>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../core/DynamicArray.hpp"
+#include "doctest/doctest.h"
+
+TEST_CASE("Testing push_back") {
+    core::DynamicArray<int> ns;
+
+    for (size_t i = 0; i < 1000; ++i) {
+        ns.push_back(i);
+    }
+
+    CHECK(ns.size() == 1000);
+    CHECK(ns[0] == 0);
+    CHECK(ns[999] == 999);
+}
