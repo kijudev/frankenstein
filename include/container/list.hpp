f53a1ac2fe16785571c18ee08bbd145103466ca2
Kiju <dev@kiju.page>
2025-10-03 12:50:49 +0200

    Add impl helper methods in List

--- include/container/list.hpp
+++ include/container/list.hpp
@@ -2,12 +2,35 @@
 // Licensed under the MIT License.
 // See LICENSE.md file in the project root for full license information.
 
+#include "../internal/scope_guard.hpp"
 #include <iterator>
 #include <memory>
 #include <type_traits>
 
 namespace frank {
-template <typename T, typename Allocator = std::allocator<T>>
+
+template <typename T>
+struct ListNode {
+    ListNode* prev {nullptr};
+    ListNode* next {nullptr};
+    T         item;
+
+    ~ListNode() = default;
+    ListNode()  = delete;
+
+    template <typename... Args>
+    ListNode(Args&&... args) noexcept(
+        std::is_nothrow_constructible_v<T, Args&&...>)
+        : item(std::forward(args)...) { }
+
+    ListNode(const ListNode&) = delete;
+    ListNode(ListNode&&)      = delete;
+
+    ListNode& operator=(const ListNode&) = delete;
+    ListNode& operator=(ListNode&&)      = delete;
+};
+
+template <typename T, typename Allocator = std::allocator<ListNode<T>>>
 class List {
 public:
     using value_type      = T;
@@ -26,11 +49,7 @@ public:
     using iterator_category      = std::bidirectional_iterator_tag;
 
 private:
-    struct Node {
-        Node*      prev;
-        Node*      next;
-        value_type value;
-    };
+    using Node = ListNode<T>;
 
     struct Impl : public Allocator {
         Node* head {nullptr};
@@ -52,6 +71,45 @@ private:
 
         Impl& operator=(const Impl&) = delete;
         Impl& operator=(Impl&&)      = delete;
+
+        [[nodiscard]] inline bool is_null() noexcept { return head == nullptr; }
+
+        [[nodiscard]] Node* allocate_node() {
+            Node* node = std::allocator_traits<Allocator>::allocate(
+                static_cast<Allocator&>(*this), 1);
+
+            return node;
+        }
+
+        template <typename... Args>
+        void construct_node(Node* node, Args&&... args) noexcept(
+            std::is_nothrow_constructible_v<Node, Args&...>) {
+            if constexpr (std::is_nothrow_constructible_v<Node, Args&...>) {
+                std::allocator_traits<Allocator>::construct(
+                    static_cast<Allocator&>(*this), std::forward(args)...);
+            } else {
+                internal::ScopeGuard guard([&]() {
+                    std::allocator_traits<Allocator>::deallocate(
+                        static_cast<Allocator&>(*this), node, 1);
+                });
+
+                std::allocator_traits<Allocator>::construct(
+                    static_cast<Allocator&>(*this), std::forward(args)...);
+
+                guard.dismiss();
+            }
+        }
+
+        void destroy_node(Node* node) noexcept(
+            std::is_nothrow_destructible_v<Node>) {
+            std::allocator_traits<Allocator>(
+                static_cast<Allocator&>(*this), node);
+        }
+
+        void deallocate_node(Node* node) noexcept {
+            std::allocator_traits<Allocator>::deallocate(
+                static_cast<Allocator&>(*this), node);
+        }
     };
 };
 }
